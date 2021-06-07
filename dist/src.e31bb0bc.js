35a22b3033a85125ecc7033265b64e865081e6c5
Luc Barbier <luc@neteck-fr.com>
2021-06-08 00:07:10 +0200

    Revert

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -213,7 +213,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "49443" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52992" + '/');
+>>>>>>> parent of d3add9a... Try fix things
 
   ws.onmessage = function (event) {
     checkedAssets = {};
